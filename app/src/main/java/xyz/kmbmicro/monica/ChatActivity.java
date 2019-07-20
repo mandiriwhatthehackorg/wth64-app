6d55cf76ce9d088d0c19369bfc99b2aaf3752477
Abdul Arfan <abdul.arfan@ruma.co.id>
2019-07-20 23:40:17 +0700

    master #time 15m #comment draft chat

--- app/src/main/java/xyz/kmbmicro/monica/ChatActivity.java
+++ app/src/main/java/xyz/kmbmicro/monica/ChatActivity.java
@@ -1,13 +1,40 @@
 package xyz.kmbmicro.monica;
 
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import co.intentservice.chatui.ChatView;
+import co.intentservice.chatui.models.ChatMessage;
+
+
 public class ChatActivity extends AppCompatActivity {
+    ChatView chatView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        chatView = (ChatView) findViewById(R.id.chat_view);
+
+
+        chatView.setOnSentMessageListener(new ChatView.OnSentMessageListener(){
+            @Override
+            public boolean sendMessage(ChatMessage chatMessage){
+                // perform actual message sending
+                chatView.addMessage(chatMessage);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        chatView.addMessage(new ChatMessage("balasan", System.currentTimeMillis(), ChatMessage.Type.RECEIVED));
+                    }
+                }, 1500);
+
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/xyz/kmbmicro/monica/MainActivity.java
+++ app/src/main/java/xyz/kmbmicro/monica/MainActivity.java
@@ -1,8 +1,11 @@
 package xyz.kmbmicro.monica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final RelativeLayout mainlayout = findViewById(R.id.mainlayout);
+
         ImageButton mainButton = findViewById(R.id.main_image_menu);
         mainButton.performClick();
         mainButton.performClick();
+//        mainButton.setOnClickListener(new View.OnClickListener() {
+//            //            @Override
+////            public void onClick(View v) {
+////            //    mainlayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+////            }
+////        });
+//
+        ImageButton monicaButton = findViewById(R.id.monica_button);
+
+        monicaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
