47f9aa718c0172412c3b8a9dd376576b5ccb5cb4
Abdul Arfan <abdul.arfan@ruma.co.id>
2019-07-20 19:39:14 +0700

    master #time 50m #comment menu addition

---
+++ app/src/main/java/xyz/kmbmicro/monica/ChatActivity.java
@@ -0,0 +1,13 @@
+package xyz.kmbmicro.monica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
--- app/src/main/java/xyz/kmbmicro/monica/MainActivity.java
+++ app/src/main/java/xyz/kmbmicro/monica/MainActivity.java
@@ -2,6 +2,7 @@ package xyz.kmbmicro.monica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton mainButton = findViewById(R.id.main_image_menu);
+        mainButton.performClick();
+        mainButton.performClick();
+
     }
 }
