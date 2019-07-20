ebd9b7918611434f30006b3a0d8ad7b8d854a8e1
Abdul Arfan <abdul.arfan@ruma.co.id>
2019-07-21 01:44:04 +0700

    master #time 5m #comment finalize button and green button active

--- app/src/main/java/xyz/kmbmicro/monica/ReplyEngine.java
+++ app/src/main/java/xyz/kmbmicro/monica/ReplyEngine.java
@@ -20,10 +20,10 @@ public class ReplyEngine {
             resultList.add("Baik, silahkan masukkan jumlah transfer");
         } else if(str.contains("100000")) {
             resultList.add("[DELAY] 1000");
-            resultList.add("Apakah anda yakin transfer ke Nomor rekening 100000917, Atas nama Abdul Arfan sebesar Rp. 100.000,- ?\n\nKetik YA atau TIDAK");
+            resultList.add("Apakah anda yakin transfer ke Nomor rekening 1000009172, Atas nama Abdul Arfan sebesar Rp. 100.000,- ?\n\nKetik YA atau TIDAK");
         } else if(str.contains("ya")) {
             resultList.add("[DELAY] 1000");
-            resultList.add("Transfer ke Nomor rekening 100000917 Atas Nama Abdul Arfan sebesar Rp. 100.000,- Berhasil\nSekarang saldo anda Rp. 4.900.000,-");
+            resultList.add("Transfer ke Nomor rekening 1000009172 Atas Nama Abdul Arfan sebesar Rp. 100.000,- Berhasil\nSekarang saldo anda Rp. 4.900.000,-");
             resultList.add("[DELAY] 1000");
             resultList.add("Apakah ada pertanyaan lain?");
         } else if(str.contains("tidak")) {
@@ -37,7 +37,7 @@ public class ReplyEngine {
             resultList.add("Baik, silahkan masukkan jumlah top-up\n1. 10000\n2. 25000\n3. 100000\n\nKetik angka");
         } else if(str.equals("1")) {
             resultList.add("[DELAY] 1000");
-            resultList.add("Top-up ke Nomor telepon 081293321203 sebesar Rp. 10.000 berhasil");
+            resultList.add("Top-up ke Nomor telepon 081293321203 sebesar Rp. 10.000 berhasil\nApakah ada pertanyaan lain?");
         } else {
             resultList.add("[DELAY] 1000");
             resultList.add("Silahkan coba lagi");
